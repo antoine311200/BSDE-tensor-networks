e05a98611e1ba812fb903f93f8e0dff8c51fc79a
antoine311200 <antoine311200@gmail.com>
2024-02-05 14:42:06 +0100

    Orthonormalization of the batch tensor train using vectorized QR decomposition

--- src/bsde_solver/tensor/tensor_train.py
+++ src/bsde_solver/tensor/tensor_train.py
@@ -6,6 +6,7 @@ from copy import deepcopy
 
 from bsde_solver.tensor.tensor_core import TensorCore
 from bsde_solver.tensor.tensor_network import TensorNetwork
+from bsde_solver.utils import batch_qr
 
 
 def left_unfold(core: TensorCore):
@@ -33,6 +34,62 @@ class BatchTensorTrain(TensorNetwork):
         self.shape = shape
         self.ranks = ranks
         self.order = len(shape)
+
+    def orthonormalize(self, mode="right", start=0, stop=-1):
+        """Orthonormalize the batch of tensor train using a series of QR decompositions.
+
+        This method is similar to the .fill method but the cores are used directly.
+
+        Args:
+            start (int, optional): Index of the first core to orthonormalize. Defaults to 0.
+            stop (int, optional): Index of the last core to orthonormalize. Defaults to -1.
+            orthogonality (str, optional): Orthogonality of the tensor train. Defaults to "right".
+
+        Raises:
+            ValueError: If the orthogonality is not either "left" or "right".
+        """
+        if stop == -1: stop = len(self.shape) - 1
+
+        if mode == "left":
+            for k in range(start, stop):
+                core_curr = self.cores[f"core_{k}"]
+                core_next = self.cores[f"core_{k+1}"]
+
+                L = core_prev.unfold(0, (1, 2), 3).view(np.ndarray)
+                R = core_curr.unfold(0, (2, 3), 1).view(np.ndarray)
+
+                V, U = batch_qr(R)
+                W = np.einsum("bij,bjk->bik", U, R)
+
+                self.cores[f"core_{k}"] = TensorCore(
+                    V.reshape(core_curr.shape),
+                    indices=core_curr.indices, name=core_curr.name
+                )
+                self.cores[f"core_{k+1}"] = TensorCore(
+                    W.reshape(core_next.shape),
+                    indices=core_next.indices, name=core_next.name
+                )
+        elif mode == "right":
+            for k in range(stop, start, -1):
+                core_prev = self.cores[f"core_{k-1}"]
+                core_curr = self.cores[f"core_{k}"]
+
+                L = core_prev.unfold(0, (1, 2), 3).view(np.ndarray)
+                R = core_curr.unfold(0, (2, 3), 1).view(np.ndarray)
+
+                V, U = batch_qr(R)
+                W = np.einsum("bij,bkj->bik", L, U)
+
+                self.cores[f"core_{k-1}"] = TensorCore(
+                    W.reshape(core_prev.shape),
+                    indices=core_prev.indices, name=core_prev.name
+                )
+                self.cores[f"core_{k}"] = TensorCore(
+                    V.T.reshape(core_curr.shape),
+                    indices=core_curr.indices, name=core_curr.name
+                )
+        else:
+            raise ValueError("orthogonality must be either 'left' or 'right'")
 
     def dupplicate(batch_size: int, tensor_train: TensorTrain):
         """Create a batch tensor train from a given tensor train by dupplicating the tensor train a given number of times.
