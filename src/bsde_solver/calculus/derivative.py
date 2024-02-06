e44d3e5c8482fb6c12a296edf68c0876e73d773f
antoine311200 <antoine311200@gmail.com>
2024-02-06 20:53:00 +0100

    Remove test from derivative computation of tensor train

--- src/bsde_solver/calculus/derivative.py
+++ src/bsde_solver/calculus/derivative.py
@@ -2,8 +2,6 @@ import numpy as np
 
 from bsde_solver.tensor.tensor_train import TensorTrain, BatchTensorTrain
 from bsde_solver.tensor.tensor_network import TensorNetwork
-from bsde_solver.tensor.tensor_core import TensorCore
-
 
 def derivative(tt: TensorTrain, phi, dphi):
     derivative = 0
@@ -86,94 +84,3 @@ def batch_derivative(tt_batch: BatchTensorTrain, phis, dphis):
         derivatives += partial_derivative.contract(batch=True).squeeze().view(np.ndarray)
 
     return derivatives
-
-if __name__ == "__main__":
-    from time import perf_counter
-
-    degree = 10
-    num_assets = 5
-
-    shape = [degree for _ in range(num_assets)]
-    ranks = [1, 3, 3, 3, 3, 1]
-    tt = TensorTrain(shape, ranks)
-    tt.randomize()
-
-    def poly(x, degree=10):
-        return np.array([x**i for i in range(degree)]).T
-
-    def poly_derivative(x, degree=10):
-        return np.array([i * x ** (i - 1) for i in range(degree)]).T
-
-    n_simulations = 5000
-    phis, dphis = [], []
-    np.random.seed(0)
-    for i in range(n_simulations):
-        x = np.random.rand(num_assets)
-
-        phi = [
-            TensorCore(
-                poly(x[i], degree=degree), name=f"phi_{i+1}", indices=(f"m_{i+1}",)
-            )
-            for i in range(tt.order)
-        ]
-        dphi = [
-            TensorCore(
-                poly_derivative(x[i], degree=degree),
-                name=f"dphi_{i+1}",
-                indices=(f"m_{i+1}",),
-            )
-            for i in range(tt.order)
-        ]
-        phis.append(phi)
-        dphis.append(dphi)
-
-    #################### Single derivative ####################
-
-    start_time = perf_counter()
-    derivatives = np.zeros(n_simulations)
-    for i in range(n_simulations):
-        derivatives[i] = derivative(tt, phis[i], dphis[i])
-
-    end_time = perf_counter() - start_time
-    print("Time:", end_time)
-
-    #################### Batch derivative ####################
-
-    phis, dphis = [], []
-    np.random.seed(0)
-    for i in range(n_simulations):
-        x = np.random.rand(num_assets)
-
-        phi = [poly(x[i], degree=degree) for i in range(tt.order)]
-        dphi = [poly_derivative(x[i], degree=degree) for i in range(tt.order)]
-        phis.append(phi)
-        dphis.append(dphi)
-    phis = np.array(phis) # (n_simulations, tt.order, degree)
-    dphis = np.array(dphis) # (n_simulations, tt.order, degree)
-
-    phis = phis.transpose((1, 0, 2)) # (tt.order, n_simulations, degree)
-    dphis = dphis.transpose((1, 0, 2)) # (tt.order, n_simulations, degree)
-    phis = [
-        TensorCore(
-            phis[i], name=f"phi_{i+1}", indices=("batch", f"m_{i+1}")
-        )
-        for i in range(tt.order)
-    ]
-    dphis = [
-        TensorCore(
-            dphis[i], name=f"dphi_{i+1}", indices=("batch", f"m_{i+1}")
-        )
-        for i in range(tt.order)
-    ]
-
-    batch_tt = BatchTensorTrain.dupplicate(n_simulations, tt)
-
-    start_time_batch = perf_counter()
-    batch_derivatives = batch_derivative(batch_tt, phis, dphis)
-    end_time_batch = perf_counter() - start_time_batch
-    print("Time:", end_time_batch)
-
-    print("Speed up factor:", end_time / end_time_batch)
-
-    # Check if the results are the same
-    print(np.allclose(derivatives, batch_derivatives))
