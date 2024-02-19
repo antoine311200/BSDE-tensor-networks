3b5c906e0ba1091f596717ea4e96fba0f900c09b
antoine311200 <antoine311200@gmail.com>
2024-02-19 18:53:20 +0100

    Clean trajectory generation file and change dot product to element wise multiplication when sigma is appropriate

--- src/bsde_solver/stochastic/path.py
+++ src/bsde_solver/stochastic/path.py
@@ -15,15 +15,25 @@ def generate_trajectories(batch_size, N, dim, X0, model, delta_t=0.01):
         X0 (np.ndarray): Initial condition.
         model (BackwardSDE): Model to simulate.
         delta_t (float, optional): Time step. Defaults to 0.01.
-        """
-    x = np.zeros((batch_size, N + 1, dim))
-    xi = np.random.normal(size=(batch_size, N, dim))
 
-    x[:, 0] = X0
+    Returns:
+        np.ndarray: Trajectories of the process.
+        np.ndarray: Noise.
+    """
+    x = np.zeros((batch_size, N + 1, dim))
+    xi = np.random.normal(size=(batch_size, N + 1, dim))
+
+    x[:, 0] = np.broadcast_to(X0, (batch_size, dim))
 
     for n in range(N):
-        x[:, n + 1] = x[:, n] + \
-            model.b(x[:, n], delta_t) * delta_t + \
-            model.sigma(x[:, n], delta_t) @ xi[:, n] * np.sqrt(delta_t)
+        sigma = model.sigma(x[:, n], n*delta_t)
+        if len(sigma.shape) != 1:
+            x[:, n + 1] = x[:, n] + \
+                model.b(x[:, n], n*delta_t) * delta_t + \
+                xi[:, n] @ sigma * np.sqrt(delta_t)
+        else:
+            x[:, n + 1] = x[:, n] + \
+                model.b(x[:, n], n*delta_t) * delta_t + \
+                (xi[:, n] * sigma) * np.sqrt(delta_t)
 
-    return x
+    return x, xi
