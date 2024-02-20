928597f5ac924b616ef0475bf0e1bb7e363126b0
antoine311200 <antoine311200@gmail.com>
2024-02-20 18:36:28 +0100

    Utility function for fast contraction of tensor train and feature maps

--- src/bsde_solver/utils.py
+++ src/bsde_solver/utils.py
@@ -14,5 +14,20 @@ def tensorized(core, shape):
 def flatten(lst):
     return [item for sublist in lst for item in sublist]
 
+from bsde_solver.core.tensor.tensor_network import TensorNetwork
+
+def fast_contract(tt, x):
+    results = []
+    # print([core.name for core in tt.cores.values()])
+    # print([name for name in tt.cores.keys()])
+    # print(tt)
+    # print(x)
+    if not isinstance(x, TensorNetwork):
+        x = TensorNetwork(cores=x, names=[f"x_{i}" for i in range(len(x))])
+    for i, (core, phi) in enumerate(zip(tt.cores.values(), x.cores.values())):
+        result = TensorNetwork(cores=[core, phi], names=[f"V_{i}", f"x_{i}"]).contract(indices=("batch", f"r_{i}", f"m_{i+1}", f"r_{i+1}"))
+        results.append(result)
+    V = TensorNetwork(cores=results, names=[f"V_{i}" for i in range(len(x.cores))]).contract(batch=True, indices=('batch',))
+    return V
 
 batch_qr = np.vectorize(np.linalg.qr, signature='(m,n)->(m,p),(p,n)')
