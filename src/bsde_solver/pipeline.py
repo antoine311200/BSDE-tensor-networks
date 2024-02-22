e3d12282b79eee98b76e5383e8d122c7c660cd34
antoine311200 <antoine311200@gmail.com>
2024-02-22 10:13:48 +0100

    Small cleaning

--- src/bsde_solver/pipeline.py
+++ src/bsde_solver/pipeline.py
@@ -20,12 +20,12 @@ batch_size = 2000
 T = 1
 sigma = 0.4
 r = 0.05
-N = 50
-num_assets = 10
+N = 100
+num_assets = 2
 dt = T / N
 
-
-rank = 2
+n_iter = 1
+rank = 1
 degree = 2
 shape = tuple([degree for _ in range(num_assets)])
 ranks = (1,) + (rank,) * (num_assets - 1) + (1,)
@@ -34,7 +34,7 @@ ranks = (1,) + (rank,) * (num_assets - 1) + (1,)
 basis = PolynomialBasis(degree)
 
 # X0 = np.zeros(num_assets) # Hamilton-Jacobi-Bellman (HJB) initial condition
-X0 = np.zeros(num_assets) # Allen-Cahn initial condition
+# X0 = np.zeros(num_assets) # Allen-Cahn initial condition
 X0 = np.array(flatten([(1, 0.5) for _ in range(num_assets//2)])) # Black-Scholes initial condition
 # X0 = -np.ones(num_assets) # Double-well HJB initial condition
 X0_batch = np.broadcast_to(X0, (batch_size, num_assets))
@@ -45,6 +45,8 @@ model = BlackScholes(X0, dt, T, r, sigma)
 # nu = np.array([0.05 for _ in range(num_assets)])
 #model =DoubleWellHJB(X0, dt, T, nu)
 pde_loss = PDELoss(model)
+
+configurations = f"{num_assets} assets | {N} steps | {batch_size} batch size | {n_iter} iterations | {degree} degree | {rank} rank"
 
 # Compute trajectories
 X, noise = generate_trajectories(batch_size, N, num_assets, X0, model, dt) # (batch_size, N + 1, num_assets)
@@ -65,9 +67,11 @@ for n in range(N + 1):
 Y = np.zeros((batch_size, N + 1))
 Y[:, -1] = model.g(X[:, -1])  # (batch_size, )
 
+start_time = time.perf_counter()
 V = [None for _ in range(N + 1)]
-V_N = multi_als(phi_X[-1], Y[:, -1], n_iter=5, ranks=ranks)
+V_N = multi_als(phi_X[-1], Y[:, -1], n_iter=n_iter, ranks=ranks)
 V[-1] = V_N
+print("Time to compute V_N:", f"{time.perf_counter() - start_time:.2f}s")
 
 check_V = fast_contract(V_N, phi_X[-1])
 
@@ -76,6 +80,10 @@ print("Prediction at N:", f"{np.mean(Y[:, -1]):.4f} | Value at N:", f"{np.mean(c
 
 print("Start")
 start_time = time.perf_counter()
+
+step_times = []
+relative_errors = []
+errors = []
 
 for n in range(N - 1, -1, -1):
     print("Step:", n)
@@ -101,35 +109,45 @@ for n in range(N - 1, -1, -1):
 
     step_n1 = h_n1*dt + Y_n1 #- np.einsum('ij,ij->i', Z_n1, noise) * np.sqrt(dt)
     # np.sum(Z_n1 @ model.sigma(X_n1, (n+1) *dt) * noise, axis=1) * np.sqrt(dt) + Y_n1
-    V_n = multi_als(phi_X_n, step_n1, n_iter=10, ranks=ranks, init_tt=V_n1)
+    V_n = multi_als(phi_X_n, step_n1, n_iter=n_iter, ranks=ranks, init_tt=V_n1)
     V[n] = V_n
     Y_n = fast_contract(V_n, phi_X_n)
 
     Y[:, n] = Y_n.view(np.ndarray).squeeze()
 
+    step_times.append(time.perf_counter() - step_start_time)
 
     price_n = model.price(X_n, n*dt)
     print("Mean reconstruction error at n:", f"{np.abs(np.mean(price_n - Y[:, n])):.2e}")
     print("Step time:", f"{time.perf_counter() - step_start_time:.2f}s")
 
-    if num_assets < 10:
-        vt = (Y[:, n + 1] - Y[:, n]) / dt
-        vx = Z_n1
-        vxx = hessian(V_n, phi_X_n, dphi_X_n, ddphi_X[n], batch=True).transpose((2, 0, 1))
-        loss = pde_loss(n*dt, X_n, Y_n, vt, vx, vxx)
-        print("Mean PDE loss", loss.mean())
-        print("Mean abs PDE loss", np.abs(loss).mean())
+    relative_errors.append(np.abs(price_n - np.mean(Y[:, n])) / price_n)
+    errors.append(np.abs(price_n - np.mean(Y[:, n])))
+
+    # if num_assets < 10:
+    #     vt = (Y[:, n + 1] - Y[:, n]) / dt
+    #     vx = Z_n1
+    #     vxx = hessian(V_n, phi_X_n, dphi_X_n, ddphi_X[n], batch=True).transpose((2, 0, 1))
+    #     loss = pde_loss(n*dt, X_n, Y_n, vt, vx, vxx)
+    #     print("Mean PDE loss", loss.mean())
+    #     print("Mean abs PDE loss", np.abs(loss).mean())
 
 print("End")
 
 end_time = time.perf_counter()
 
 print(f"Time: {end_time - start_time:.2f}s")
+print(f"Mean step time: {np.mean(step_times):.2f}s")
 
 print()
-print("Price at 0", np.mean(model.price(X0_batch, 0)))
+price = np.mean(model.price(X0_batch, 0))
+print("Price at 0", price)
 print("Predicted Price:", np.mean(Y[:, 0]))
 
+print(Y[:, 0])
+
+# Print relative error in percentage
+print("Relative error:", f"{np.abs(price - np.mean(Y[:, 0])) / price * 100:.2f}%")
 
 plt.figure(figsize=(10, 5))
 n_simulations = 3
@@ -147,5 +165,20 @@ plt.scatter([0], [np.mean(Y[:, 0])], color="red", label="Predicted Price at 0", 
 plt.scatter([0], [np.mean(model.price(X0_batch, 0))], color="red", label="Ground Price at 0", marker="o")
 plt.xlabel("Time")
 plt.ylabel("Price")
+plt.title(f"Evolutions of prices | {configurations}")
 plt.legend()
+plt.show()
+
+plt.figure(figsize=(10, 5))
+plt.plot(np.arange(N + 1), relative_errors[::-1])
+plt.xlabel("Time")
+plt.ylabel("Relative error")
+plt.title(f"Relative error | {configurations}")
+plt.show()
+
+plt.figure(figsize=(10, 5))
+
+plt.xlabel("Time")
+plt.ylabel("Error")
+plt.title(f"Error | {configurations}")
 plt.show()
